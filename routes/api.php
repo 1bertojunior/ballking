bbbf8ab6f740234aff9033fd8f0aae291e6d439e
Humberto Júnior <hjunior854@gmail.com>
2024-06-21 21:38:35 -0300

    feat(routes): add cities API resource route with Sanctum auth middleware

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cities', CityController::class)->middleware('auth:sanctum');
+
 
 require __DIR__.'/auth.php';
