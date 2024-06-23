5bccf5741c28d14402216d51abfd15da0a53308b
Humberto Júnior <hjunior854@gmail.com>
2024-06-23 14:33:10 -0300

    refactor: rename name of tables in all methods downs in all migrations

--- database/migrations/2024_06_23_122530_create_championship_edition_table.php
+++ database/migrations/2024_06_23_122530_create_championship_edition_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('championship_edition');
+        Schema::dropIfExists('championship_editions');
     }
 };
