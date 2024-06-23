7b859d9723856b9e72d74e67364a145ec2185d79
Humberto Júnior <hjunior854@gmail.com>
2024-06-23 14:37:12 -0300

    feat: add soft deletes to positions table

--- database/migrations/2024_06_23_132610_create_position_table.php
+++ database/migrations/2024_06_23_132610_create_position_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name',25)->unique();
             $table->char('abb', 3)->unique();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
